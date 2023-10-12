e3744191cbd6e799bd0fc2ff9716cc4b6411dfb3
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2023-10-12 17:58:10 +0800

    update on prod

--- src/chat_proper/UserRegister.jsx
+++ src/chat_proper/UserRegister.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import bg_image from '../assets/animal-crossing.jpg';
+import logo_img from '../assets/logo.png'; 
 import { useState } from "react";
 
 export default function UserRegister({registrationResponse, registerUser}){
@@ -19,19 +20,28 @@ export default function UserRegister({registrationResponse, registerUser}){
         lg:w-[20%] lg:h-[50%]
         md:w-[60%] md:h-[60%]">
           <img src={bg_image} className='w-full h-full absolute'/>
-          <div className='bg-primary-variant w-full h-full p-5 bg-clip-padding backdrop-filter backdrop-blur-none bg-opacity-50 lg:p-6 md:p-10 flex flex-col gap-10 justify-center items-center'>
-            <h1 className='font-bold text-center w-full text-accent'>
-              Please Enter Your Name:
-            </h1>
-            <div className='flex flex-col gap-1 w-full'>
-              <input type="text" className="border-[1px] rounded-lg w-full p-2" name='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
-              {
-                registrationResponse != null ? 
-                key == "Fail" ? <span className='text-red-500'>{message}</span> : <></>
-                : <></>
-              }
+          <div className='bg-primary-variant w-full h-full bg-clip-padding backdrop-filter backdrop-blur-none bg-opacity-50'>
+            <div className='w-full flex h-[20%] justify-center'>
+              <img src={logo_img}/>
+              <span>
+                Crossing Pub
+              </span>
             </div>
-            <button className="w-full border-[1px] border-accent rounded-md bg-secondary-color p-2 shadow-md text-accent transition-all hover:border-secondary-color hover:shadow-secondary-color hover:bg-accent hover:text-secondary-color" onClick={() => registerUser(username)}>Submit</button>
+            <div className='p-5 lg:p-6 md:p-10 flex flex-col gap-10 justify-center items-center h-[80%]'>
+              <h1 className='font-bold text-center w-full text-accent'>
+                Please Enter Your Name:
+              </h1>
+              <div className='flex flex-col gap-1 w-full'>
+                <input type="text" className="border-[1px] rounded-lg w-full p-2" name='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                  {
+                    registrationResponse != null ? 
+                    key == "Fail" ? <span className='text-red-500'>{message}</span> : <></>
+                    : <></>
+                  }
+              </div>
+              <button className="w-full border-[1px] border-accent rounded-md bg-secondary-color p-2 shadow-md text-accent transition-all hover:border-secondary-color hover:shadow-secondary-color hover:bg-accent hover:text-secondary-color" onClick={() => registerUser(username)}>Submit</button>
+            </div>
+            
           </div>
           
         </div>
