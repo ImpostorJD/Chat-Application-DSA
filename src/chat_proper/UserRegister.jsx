4741ceb7d3be193f5acfe54844bbd12b8addb6ba
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2023-10-12 18:22:45 +0800

    added a bit of padding

--- src/chat_proper/UserRegister.jsx
+++ src/chat_proper/UserRegister.jsx
@@ -21,7 +21,7 @@ export default function UserRegister({registrationResponse, registerUser}){
         md:w-[60%] md:h-[60%]">
           <img src={bg_image} className='w-full h-full absolute'/>
           <div className='bg-primary-variant w-full h-full bg-clip-padding backdrop-filter backdrop-blur-none bg-opacity-50'>
-            <div className='w-full flex h-[20%] justify-center items-center gap-2'>
+            <div className='w-full flex h-[20%] justify-center items-center gap-1 p-1'>
               <img src={logo_img} className='rounded-full border-[1px] w-auto border-black h-full'/>
               <span className='text-[2rem] font-bold'>
                 Crossing Pub
