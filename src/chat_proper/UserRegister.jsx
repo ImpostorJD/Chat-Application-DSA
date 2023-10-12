a07e5079628ac3a3a47b6d938d0a2dcb3bdd9065
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2023-10-12 18:19:26 +0800

    AYAW KO NA TALAGA

--- src/chat_proper/UserRegister.jsx
+++ src/chat_proper/UserRegister.jsx
@@ -22,7 +22,7 @@ export default function UserRegister({registrationResponse, registerUser}){
           <img src={bg_image} className='w-full h-full absolute'/>
           <div className='bg-primary-variant w-full h-full bg-clip-padding backdrop-filter backdrop-blur-none bg-opacity-50'>
             <div className='w-full flex h-[20%] justify-center items-center gap-2'>
-              <img src={logo_img} className='rounded-full border-[1px] w-[7rem] border-black h-[7rem]'/>
+              <img src={logo_img} className='rounded-full border-[1px] w-auto border-black h-full'/>
               <span className='text-[2rem] font-bold'>
                 Crossing Pub
               </span>
